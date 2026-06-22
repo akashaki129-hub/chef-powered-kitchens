650da74259eb062cde9d19d70bd4379888735b5a
akashaki129-hub <akashaki129@gmail.com>
2026-06-22 15:49:40 +0530

    Enable Vercel visitor analytics (#6)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import {
   Outlet,
   Link,
@@ -147,6 +148,7 @@ function RootComponent() {
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <Toaster />
+      <Analytics />
     </QueryClientProvider>
   );
 }
